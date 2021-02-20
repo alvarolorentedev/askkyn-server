dbc42a9c0bd6f5ea78c26e0c8d22322f20abbcf6
kanekotic <me@kanekotic.com>
2021-02-20 10:11:04 +0100

    Alvaro | add cors to heroku

--- src/server.ts
+++ src/server.ts
@@ -16,16 +16,18 @@ const stream = new MyStream();
 
 const app = express();
 const whitelist = [
-  "http://127.0.0.1:8000",
-  "https://127.0.0.1:8000",
-  "http://localhost:8000",
-  "https://localhost:8000",
-  "http://localhost:3000",
-  "https://localhost:3000"
+  'http://127.0.0.1:8000',
+  'https://127.0.0.1:8000',
+  'http://localhost:8000',
+  'https://localhost:8000',
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'https://www.askkyn.com',
 ];
-app.use(cors({
+app.use(
+  cors({
     origin: whitelist,
-    credentials: true
+    credentials: true,
   })
 );
 
