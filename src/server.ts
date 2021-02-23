db48e7fe3a25f6e32046dcbddfca934343e53e1a
kanekotic <me@kanekotic.com>
2021-02-23 09:38:05 +0100

    Alvaro | try fix pg connection

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,9 @@ app.use(morgan('tiny', { stream }));
 app.use('/db', expressPouchDb(sqlPouchDB));
 
 const dbConnection =
-  process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : 'test.db';
+  process.env.NODE_ENV === 'production'
+    ? 'pg://wawvkqbphmvavu:a60dd0e1b5cc6655a950c1a21239e38872678c9424b40f418346757131873c8f@ec2-54-247-158-179.eu-west-1.compute.amazonaws.com:5432/det1ho3dnt28s0'
+    : 'test.db';
 
 const myPouchInstance = sqlPouchDB(dbConnection);
 
